605ab414864ff9ea0ed844283435b98d3945419e
Banto Balazs <balazs.banto@agroninja.com>
2021-11-12 18:49:15 +0100

    Initial commit

---
+++ ToasterLibrary/src/main/java/com/blade/toasterlibrary/ToasterMessage.kt
@@ -0,0 +1,13 @@
+package com.blade.toasterlibrary
+
+import android.content.Context
+import android.widget.Toast
+
+class ToasterMessage {
+    companion object {
+        fun show(context:Context, message:String) {
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+        }
+
+    }
+}
